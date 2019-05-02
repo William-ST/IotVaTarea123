1f8b60aebccd8d65b8e79c7611143fdc76237bb5
William Sulca <sulca.03.1995@gmail.com>
2019-05-02 00:59:26 -0500

    add base0 add base2 add base3 add base4

---
+++ base0/src/main/java/com/cursoandroid/base0/MainActivity.java
@@ -0,0 +1,103 @@
+package com.cursoandroid.base0;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private Mat input_gray;
+    private static final String TAG = "OCVSample::Activity";
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+            }
+        }
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.base0_activity_java_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null) mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null) mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return null;
+    }
+
+    public MainActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
+
+
+}
---
+++ base2/src/main/java/com/cursoandroid/base2/MainActivity.java
@@ -0,0 +1,181 @@
+package com.cursoandroid.base0;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private Mat input_gray;
+    private static final String TAG = "OCVSample::Activity";
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private int indiceCamara; // 0-> camara trasera; 1-> camara frontal
+    private int cam_anchura = 320;// resolucion deseada de la imagen
+    private int cam_altura = 240;
+    private static final String STATE_CAMERA_INDEX = "cameraIndex";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+            }
+        }
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.base0_activity_java_surface_view);
+        if (savedInstanceState != null) {
+            indiceCamara = savedInstanceState.getInt(STATE_CAMERA_INDEX, 0);
+        } else {
+            indiceCamara = CameraBridgeViewBase.CAMERA_ID_BACK;
+        }
+        mOpenCvCameraView.setCameraIndex(indiceCamara);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        openOptionsMenu();
+        return true;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the current camera index.
+        savedInstanceState.putInt(STATE_CAMERA_INDEX, indiceCamara);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.cambiarCamara:
+                if (indiceCamara == CameraBridgeViewBase.CAMERA_ID_BACK) {
+                    indiceCamara = CameraBridgeViewBase.CAMERA_ID_FRONT;
+                } else indiceCamara = CameraBridgeViewBase.CAMERA_ID_BACK;
+                recreate();
+                break;
+            case R.id.resolucion_800x600:
+                cam_anchura = 800;
+                cam_altura = 600;
+                reiniciarResolucion();
+                break;
+            case R.id.resolucion_640x480:
+                cam_anchura = 640;
+                cam_altura = 480;
+                reiniciarResolucion();
+                break;
+            case R.id.resolucion_320x240:
+                cam_anchura = 320;
+                cam_altura = 240;
+                reiniciarResolucion();
+                break;
+        }
+        String msg = "W=" + Integer.toString(cam_anchura) + " H= " +
+                Integer.toString(cam_altura) + " Cam= " +
+                Integer.toBinaryString(indiceCamara);
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    public void reiniciarResolucion() {
+        mOpenCvCameraView.disableView();
+        mOpenCvCameraView.setMaxFrameSize(cam_anchura, cam_altura);
+        mOpenCvCameraView.enableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        cam_altura = height; //Estas son las que se usan de verdad
+        cam_anchura = width;
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG, "OpenCV se cargo correctamente");
+                    mOpenCvCameraView.setMaxFrameSize(cam_anchura, cam_altura);
+                    mOpenCvCameraView.enableView();
+                }
+                break;
+                default: {
+                    Log.e(TAG, "OpenCV no se cargo");
+                    Toast.makeText(MainActivity.this, "OpenCV no se cargo", Toast.LENGTH_LONG).show();
+                    finish();
+                    break;
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null) mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null) mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return null;
+    }
+
+    public MainActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
+
+
+}
